69f5d19ec1a19b35abaed48a709567073b5efee6
Zahidur Rahman Faisal <d.m.zahidur.rahman@ihis.com.sg>
2022-01-30 04:13:22 +1100

    chore: Added TODO for Chapter 7
    Chapter 7: Debugging Databases

--- app/src/main/java/com/raywenderlich/podplay/db/PodcastDao.kt
+++ app/src/main/java/com/raywenderlich/podplay/db/PodcastDao.kt
@@ -51,6 +51,7 @@ interface PodcastDao {
   @Query("SELECT * FROM Podcast ORDER BY FeedTitle")
   suspend fun loadPodcastsStatic(): List<Podcast>
 
+  // TODO: Chapter 7 - Fix erroneous database query while loading episodes
   @Query("SELECT * FROM Episode WHERE podcastId = :podcastId ORDER BY releaseDate DESC")
   suspend fun loadEpisodes(podcastId: Long): List<Episode>
 
